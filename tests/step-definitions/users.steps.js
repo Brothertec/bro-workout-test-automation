e74acd5b82d1c8ed912020c5ab15733575be2b60
Guilherme Campos <guilherme.campos.dev@gmail.com>
2026-09-16 14:58:15 -0300

    test: automate required fields validation for HU002

--- tests/step-definitions/users.steps.js
+++ tests/step-definitions/users.steps.js
@@ -1,5 +1,24 @@
 const { Given, When, Then } = require('@cucumber/cucumber');
 
+Given('The user is on the users page', async function () {
+    await this.usersPage.goto();
+});
+
+Given('The user opens the user creation form', async function () {
+    await this.usersPage.openUserCreationForm();
+});
+
+When(
+    'The user fills the user form without the {string} field',
+    async function (field) {
+        await this.usersPage.fillUserFormWithout(field);
+    },
+);
+
+When('The user submits the user creation form', async function () {
+    await this.usersPage.submitUserCreationForm();
+});
+
 When('I go to users page by navBar', async function (){
     await this.homePage.goToUsersNavBar();
 })
@@ -11,3 +30,13 @@ When('I go to users page by card', async function (){
 Then('I should see a user and his features', async function () {
     await this.usersPage.userVerify();
 });
+
+Then(
+    'The user should see {string} below the {string} field',
+    async function (message, field) {
+        await this.usersPage.requiredMessageShouldBeVisibleBelowField(
+            field,
+            message,
+        );
+    },
+);
